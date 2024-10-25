19b190a012fa6fd3ba9108a8c18c1a20086c0b8a
Brian Song <brian.cs.song@gmail.com>
2024-10-25 19:49:48 +0000

    Finish

--- app.rb
+++ app.rb
@@ -34,7 +34,12 @@ get("/multiply") do
   erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num * @second_num
+  
   erb(:mult_result)
 end
 
@@ -43,5 +48,10 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num / @second_num
+
   erb(:div_result)
 end
